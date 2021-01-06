042a9c25d5a181ff7219dffe867099455fb0a20a
KevinKaras <kevinkaras2@gmail.com>
2021-01-06 15:40:17 -0600

    submit now works, fills license

--- spud-face.js
+++ spud-face.js
@@ -1,4 +1,18 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+      let formField = document.querySelectorAll('.form__input');
+      let formContainer = document.querySelector("#drivers-license-form");
+      let driversInfo = document.querySelectorAll('.license__info');
+      let formValues = [];
+      // formContainer.addEventListener('input', event =>{
+
+      //   for (let i = 0; i < formField.length - 3; i++){
+
+      //     formValues.push(formField[i].values);
+      //   }
+      //   console.log(formValues[0]);
+      // });
+      
+
 
   // ** Phase 1B: Update license with event delegation and event.target ** 
 
@@ -10,6 +24,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
  
 
   // ** Phase 4: Update submit button click count **
+      document.addEventListener('submit', event =>{
+        event.preventDefault();
+        for (let i = 0; i < formField.length - 3; i++){
+          driversInfo[i].innerText = formField[i].value;
+        }
+      })
+
 
  
 });
