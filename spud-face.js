eba52e37d06a868007aca3b9d830583eb1645b4b
KevinKaras <kevinkaras2@gmail.com>
2021-01-06 17:34:30 -0600

    check button updated finished

--- spud-face.js
+++ spud-face.js
@@ -1,9 +1,14 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  let formField = document.querySelectorAll('.form__input');
-  let formContainer = document.querySelector("#drivers-license-form");
-  let driversInfo = document.querySelectorAll('.license__info');
-  let formValues = [];
+  // let formField = document.querySelectorAll('.form__input');
+  // let formContainer = document.querySelector("#drivers-license-form");
+  // let driversInfo = document.querySelectorAll('.license__info');
+  // let formValues = [];
+  let driver1 = document.querySelector('#license-num');
+  let driver2 = document.querySelector('#license-num-confirm');
   let entireForm = document.querySelector("#drivers-license-form");
+  document.querySelector('#card-donor-status').innerText = "No";
+  
+
   // formContainer.addEventListener('input', event =>{
 
   //   for (let i = 0; i < formField.length - 3; i++){
@@ -22,11 +27,26 @@ window.addEventListener("DOMContentLoaded", (event) => {
       document.querySelector("#card-title").innerText = event.target.value;
     } else if (event.target.id === "first-name") {
       document.querySelector("#card-first-name").innerText = event.target.value;
+    } else if (event.target.id === "last-name"){
+      document.querySelector("#card-last-name").innerText = event.target.value;
+    } else if (event.target.id === "eye-color"){
+      document.querySelector("#card-eye-color").innerText = event.target.value;
+    } else if (event.target.id === "height"){
+      document.querySelector("#card-height").innerText = event.target.value;
+    } else if (driver1.value === driver2.value){
+      document.querySelector('#card-license-num').innerText = driver2.value;
+    } else if (driver1.value !== driver2.value){
+      document.querySelector('#card-license-num').innerText = "";
     }
-
+    if (event.target.checked === true){
+      document.querySelector('#card-donor-status').innerText = "Yes";
+    } else if (event.target.checked === false) {
+      document.querySelector('#card-donor-status').innerText = "No";
+    } 
   });
 
   // ** Phase 2: Add focus and blur events to form inputs **
+
 
 
   // ** Phase 3: Check that license numbers match **
